8de214700732b44c454d1d0c5b7cacf468fb58a1
DSwojak <79580203+DSwojak@users.noreply.github.com>
2021-02-24 13:22:36 +0100

    Add files via upload

---
+++ PPJ/PPJ_3.java
@@ -0,0 +1,28 @@
+public class PPJ_3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Zadanie 1:"+"\n");
+    Z1();
+        System.out.println("\n"+"Zadanie 2:"+"\n");
+    Z2();
+        System.out.println("\n"+"Zadanie 3:"+"\n");
+    Z3();
+
+    }
+
+    static void Z1() {
+        System.out.println("Witaj PPJ");
+    }
+
+    static void Z2() {
+        System.out.println("DEC: "+128+" BIN: "+0b10000000+" OCT: "+0200+" HEX: "+0x80);
+        System.out.println("DEC: "+1128+" BIN: "+0b010011001100+" OCT: "+02314+" HEX: "+0x4CC);
+        System.out.println("DEC: "+2775+" BIN: "+0b101011010111+" OCT: "+05327+" HEX: "+0xAD7);
+    }
+
+    static void Z3() {
+        System.out.println("DEC: "+150+" BIN: "+0b10010110+" OCT: "+0226+" HEX: "+0x96);
+    }
+
+}
